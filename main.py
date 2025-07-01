20142792317de4b0967d6b6542249bf1a7128e0b
Letsktor <vajasmate2013@gmail.com>
2025-07-01 14:23:45 +0200

    finished the Player

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 
 import pygame
 from constants import *
-from palyer import Player
+from player import Player
 def main():
     pygame.init()
     screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_WIDTH))
     clock=pygame.time.Clock()
     dt=0
+    updatable=pygame.sprite.Group()
+    drawable=pygame.sprite.Group()
+    Player.containers=(updatable,drawable)
+
     player=Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
+    
     while True:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
+        
         pygame.display.flip()
         dt=clock.tick(60)/1000
     
